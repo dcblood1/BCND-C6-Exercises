4a3c584f3df2fae6b8569b128ec24f54db7a3d6f
dcblood1 <dcblood1@gmail.com>
2021-05-07 08:36:36 -0500

    completed
    completed exercises

--- contracts/ExerciseC6B.sol
+++ contracts/ExerciseC6B.sol
@@ -42,10 +42,47 @@ contract ExerciseC6B {
         _;
     }
 
+    /**
+    * @dev modifier that requires the rate limiter to be in effect, limiting how often a function can be called
+     */
+     uint256 private enabled = block.timestamp;
+     modifier rateLimit(uint time) {
+         require(block.timestamp >= enabled, "Rate limiting in effect"); //require timestamp to be greater than enabled
+         enabled = enabled.add(time); // increase enabled time using timestamp
+         _;
+     }
+
+    // this prevents bad players from calling your function multiple times without letting it completel
+    // typically for people who found a bug and want to drain accounts with a loop of transactions
+    // essentially it might loop through the whole thing, but then at the end, it will fail, killing the whole transaction
+     uint256 private counter = 1;
+     modifier entrancyGuard(){
+        counter = counter.add(1);
+        uint256 guard = counter;
+         _;
+         require(guard == counter, "That is not allowed");
+     }
+
     /********************************************************************************************/
     /*                                     SMART CONTRACT FUNCTIONS                             */
     /********************************************************************************************/
 
+    // this is going to be my safe withdraw function
+    mapping(address => uint256) private sales; //maps addresses to sales
+    // dont know if this 30 minutes is the correct way to use Ratelimit, think it would be timestamp one
+    function safeWithdraw(uint256 amount) external rateLimit(30 minutes) entrancyGuard {
+
+        //checks
+        require(msg.sender == tx.origin, "Contracts not allowed"); //msg.sender is caller of function, tx.origin is for this one a contract??
+        require(sales[msg.sender] >= amount, "insufficient funds"); 
+
+        //effects
+        uint256 amount = sales[msg.sender]; //set local var to amount
+        sales[msg.sender] = sales[msg.sender].sub(amount); //subtract sales amount usign safemath
+        
+        //interaction
+        msg.sender.transfer(amount); //send that money
+    }
 
     
 }
