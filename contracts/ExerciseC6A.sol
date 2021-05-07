99a998eccf276d02c7c76c8a11106c0a758876bc
dcblood1 <dcblood1@gmail.com>
2021-05-07 08:37:10 -0500

    completed
    completed tasks

--- contracts/ExerciseC6A.sol
+++ contracts/ExerciseC6A.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.25;
+pragma solidity ^0.4.26;
 
 contract ExerciseC6A {
 
@@ -6,7 +6,7 @@ contract ExerciseC6A {
     /*                                       DATA VARIABLES                                     */
     /********************************************************************************************/
 
-
+    uint constant M = 2; //M of N for multi party sig
     struct UserProfile {
         bool isRegistered;
         bool isAdmin;
@@ -15,6 +15,9 @@ contract ExerciseC6A {
     address private contractOwner;                  // Account used to deploy contract
     mapping(address => UserProfile) userProfiles;   // Mapping for storing user profiles
 
+    bool private operational = true; // operational for stop loss 
+
+    address[] multiCalls = new address[](0); // used to track all addresses that call consensus
 
 
     /********************************************************************************************/
@@ -51,6 +54,17 @@ contract ExerciseC6A {
         _;
     }
 
+        /**
+    * @dev Modifier that requires the "operational" boolean variable to be "true"
+    *      This is used on all state changing functions to pause the contract in 
+    *      the event there is an issue that needs to be fixed
+    */
+    modifier requireIsOperational() 
+    {
+        require(operational, "Contract is currently not operational");
+        _;  // All modifiers require an "_" which indicates where the function body will be added
+    }
+
     /********************************************************************************************/
     /*                                       UTILITY FUNCTIONS                                  */
     /********************************************************************************************/
@@ -72,6 +86,21 @@ contract ExerciseC6A {
         return userProfiles[account].isRegistered;
     }
 
+        /**
+    * @dev Get operating status of contract
+    *
+    * @return A bool that is the current operating status
+    */      
+    function isOperational() 
+                            public 
+                            view 
+                            returns(bool) 
+    {
+        return operational;
+    }
+
+
+
     /********************************************************************************************/
     /*                                     SMART CONTRACT FUNCTIONS                             */
     /********************************************************************************************/
@@ -82,6 +111,7 @@ contract ExerciseC6A {
                                     bool isAdmin
                                 )
                                 external
+                                requireIsOperational
                                 requireContractOwner
     {
         require(!userProfiles[account].isRegistered, "User is already registered.");
@@ -90,6 +120,41 @@ contract ExerciseC6A {
                                                 isRegistered: true,
                                                 isAdmin: isAdmin
                                             });
+    }
+
+        /**
+    * @dev Sets contract operations on/off
+    *
+    * When operational mode is disabled, all write transactions except for this one will fail
+    */    
+    function setOperatingStatus
+                            (
+                                bool mode
+                            ) 
+                            external
+                            //requireIsOperational look at this kind of bug! requires to be operational when setting, so would lock you out! $$$ wasted forever.
+                            //requireContractOwner cannot have this if using mult-sig, no multiple owners 
+    {
+        //operational = mode; // this line ALONE when setting operational mode, and controlled by one admin
+        require(mode != operational, "New mode must be different from existing mode"); //fail fast, if trying to change something but it's not different, doesn't cost us more gas
+        require(userProfiles[msg.sender].isAdmin, "Caller is not an admin"); //have to be an admin to change operation
+
+
+        bool isDuplicate = false; //makes sure one admin is not calling it multiple times
+        for(uint c=0; c<multiCalls.length; c++) { //normally don't want to iterate through an array, due to potential high gas costs
+            if (multiCalls[c] == msg.sender) {
+                isDuplicate = true;
+                break;
+            }
+        }
+        require(!isDuplicate, "Caller has already called this function.");
+
+        multiCalls.push(msg.sender); //add admin to array to change operational mode, if array long enough, enough admins want the change
+        if (multiCalls.length >= M) {
+            operational = mode;      
+            multiCalls = new address[](0); //reinitialize multicalls, dont forget this step      
+        }
+
     }
 }
 
--- test/ExerciseC6A.js
+++ test/ExerciseC6A.js
@@ -11,8 +11,8 @@ contract('ExerciseC6A', async (accounts) => {
   it('contract owner can register new user', async () => {
     
     // ARRANGE
-    let caller = accounts[1]; // This should be config.owner or accounts[0] for registering a new user
-    let newUser = config.testAddresses[0]; 
+    let caller = accounts[0]; // This should be config.owner or accounts[0] for registering a new user
+    let newUser = config.testAddresses[0];  //test addresses are random, acconts[i] are associated with ganache
 
     // ACT
     await config.exerciseC6A.registerUser(newUser, false, {from: caller});
@@ -22,6 +22,34 @@ contract('ExerciseC6A', async (accounts) => {
     assert.equal(result, true, "Contract owner cannot register new user");
 
   });
+
+  it('function call is made when multi-party threshold is reached', async () => {
+    
+    // ARRANGE
+    let admin1 = accounts[1]; //accounts are associated with ganache, 
+    let admin2 = accounts[2];
+    let admin3 = accounts[3];
+    let admin4 = accounts[4];
+    
+    await config.exerciseC6A.registerUser(admin1, true, {from: config.owner});
+    await config.exerciseC6A.registerUser(admin2, true, {from: config.owner});
+    await config.exerciseC6A.registerUser(admin3, true, {from: config.owner});
+    await config.exerciseC6A.registerUser(admin4, true, {from: config.owner});
+    
+    let startStatus = await config.exerciseC6A.isOperational.call(); //get current status of contract
+    let changeStatus = !startStatus; //change it to opposite of above. Better than static bc this test is testing if it worked either way
+
+    // ACT
+    await config.exerciseC6A.setOperatingStatus(changeStatus, {from: admin1});
+    await config.exerciseC6A.setOperatingStatus(changeStatus, {from: admin2});
+    
+    let newStatus = await config.exerciseC6A.isOperational.call(); 
+
+    // ASSERT
+    assert.equal(changeStatus, newStatus, "Multi-party call failed");
+
+  });
+
 
  
 });
